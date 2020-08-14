7257278e0a5c42cd7b42b8acb50278a6149b7039
Calvin Tai <jfctai@rams.colostate.edu>
2020-08-14 17:39:36 -0600

    env setup for Linux

--- ecommerce/test.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-// Using the factory defaults.
-crypto.scrypt('secret', 'salt', 64, (err, derivedKey) => {
-    if (err) throw err;
-    console.log(derivedKey.toString('hex'));  // '3745e48...08d59ae'
-});
